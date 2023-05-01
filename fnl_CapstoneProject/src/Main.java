be8059ab67ebcf7edeca7686217d62b2dc9a55c0
Ojas Khandelwal <112968162+KhandelwalOjas@users.noreply.github.com>
2023-05-01 09:01:46 -0700

    test 1

--- fnl_CapstoneProject/src/Main.java
+++ fnl_CapstoneProject/src/Main.java
@@ -1,10 +1,56 @@
+// This class does nothing
+// it is created to establish the GitHub
+// repository.
+import java.awt.*; 
+import java.awt.event.*;
+import javax.swing.*;
 
 
-public class Main {
-
+public class Main extends JPanel implements ActionListener {
+	
+	private int time;
+	private int xCoord, yCoord; 
+	
+	
+	
+	public Main () {
+		time = 0;
+		Timer clock = new Timer(30, this);
+		clock.start();
+		xCoord = 250;
+		yCoord = getHeight();
+			
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		setBackground(Color.WHITE);
+		
+		
+		super.paintComponent(g);
+		
+		g.setColor(Color.ORANGE);
+		g.fillOval(xCoord, yCoord, 50, 50);
+		
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		time++;
+		yCoord--;
+		repaint();
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame w = new JFrame("Trial");
+		w.setBounds(500,250,500,500);
+		
+		Container c = w.getContentPane();
+		c.add(new Main());
+		w.setVisible(true);
+		
+		
 	}
 
 }
