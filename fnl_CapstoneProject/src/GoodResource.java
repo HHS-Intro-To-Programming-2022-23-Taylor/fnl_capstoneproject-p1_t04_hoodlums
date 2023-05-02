1d89f80d5e0cb22395c836d1109fa58c2a094db3
Ojas Khandelwal <112968162+KhandelwalOjas@users.noreply.github.com>
2023-05-02 09:24:09 -0700

    classes setup

---
+++ fnl_CapstoneProject/src/GoodResource.java
@@ -0,0 +1,35 @@
+import java.awt.Image;
+
+public class GoodResource implements Resources{
+
+	@Override
+	public int returnPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double createPath() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Image returnPic() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
