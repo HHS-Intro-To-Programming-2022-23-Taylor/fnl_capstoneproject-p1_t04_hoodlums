d84fceaaabb5e458393a916fdf89fdad7bd6e42d
Bharath Jayadev <112968125+jayadevbharath@users.noreply.github.com>
2023-05-04 08:51:12 -0700

    updates

---
+++ fnl_CapstoneProject/src/GoodResource.java
@@ -0,0 +1,45 @@
+import java.awt.Image;
+
+public class GoodResource implements Resources{
+	
+	private int points;
+	
+	private double xCoord, yCoord;
+	
+	private Image solar, wind, hydro;
+	
+	public GoodResource() {
+		
+	}
+	
+	@Override
+	public int returnPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double createPath() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Image returnPic() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ fnl_CapstoneProject/src/Resources.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public interface Resources {
+	
+	public int returnPoints();
+	public double getX();
+	public double getY();
+	public double createPath();
+	public Image returnPic();
+
+}
